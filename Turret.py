8d3255cd7cce623923d7e45e6deb438ba2d27787
Tim Henkins <ethanrgroves4@gmail.com>
2020-03-04 18:35:09 -0800

    Getting ready for vision
    Added flywheel PID, changed auxiliary to the xbox,  cleaned up general stuff

--- Turret.py
+++ Turret.py
@@ -1,36 +1,54 @@
 import wpilib
 import rev
 
-class Turret: #this is currently just manual control, but motors can only be instantiated once so
-	def __init__(self, turretID, flywheelID): #eventually all the turret stuff will go here
+class Turret:
+	def __init__(self, turretID, flywheelID):
 		self.turretMotor = rev.CANSparkMax(turretID, rev.MotorType.kBrushless)
 		self.flywheelMotor = rev.CANSparkMax(flywheelID, rev.MotorType.kBrushless)
+		
 		self.turretEncoder = self.turretMotor.getEncoder()
 		self.flywheelEncoder = self.flywheelMotor.getEncoder()
 		
-		self.turretEncoder.setPositionConversionFactor(12.414)
+		self.flywheelMotor.setInverted(True)
+		self.flywheelMotor.enableVoltageCompensation(12)
+		
+		self.turretEncoder.setPositionConversionFactor(12.414) #this definitely needs to change
 		
 		kPTurret = .007
 		kITurret = .06
 		kDTurret = 0
 		kIZoneTurret = 3 #degrees in which position error is integrated
-		turretMaxOutput = .25
-		turretMinOutput = .25
+		turretMinOutput = -1
+		turretMaxOutput = 1
 		
-		self.turretTolerance = .3 #degrees
+		kPFlywheel = .0001
+		kIFlywheel = 0
+		kDFlywheel = 0
+		self.kSFlywheel = .101
+		kFFFlywheel = .0001751
+		flywheelMinOutput = -1
+		flywheelMaxOutput = 1
+		
+		self.turretTolerance = .25 #degrees
 		
 		self.turretController = self.turretMotor.getPIDController()
 		self.turretController.setP(kPTurret)
 		self.turretController.setI(kITurret)
 		self.turretController.setD(kDTurret)
-		self.turretController.setIZone(kIZoneTurret)
 		self.turretController.setOutputRange(turretMinOutput,turretMaxOutput)
 		
-		#self.lights = wpilib.PWM(0)
+		self.flywheelController = self.turretMotor.getPIDController()
+		self.flywheelController.setP(kPFlywheel)
+		self.flywheelController.setI(kIFlywheel)
+		self.flywheelController.setD(kDFlywheel)
+		self.flywheelController.setFF(kFFFlywheel)
+		self.flywheelController.setOutputRange(flywheelMinOutput,flywheelMaxOutput)
 		
 	def flywheelManual(self,speed):
-		speed *= -1
-		self.flywheelMotor.set(speed)
+		self.flywheelMotor.setVoltage(speed)
+		
+	def flywheelRPM(self,rpm):
+		self.flywheelController.setReference(rpm,rev.ControlType.kVelocity,0,self.kSFlywheel)
 		
 	def turretManual(self,speed):
 		self.turretMotor.set(speed)
